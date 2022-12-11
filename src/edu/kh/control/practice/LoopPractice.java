76a3dc209c934702734c4fd08e8b4f5ada9b9e7a
DESKTOP-HE6SEOU\arm <suj000777@gmail.com>
2022-12-11 21:15:51 +0900

    commit 5

--- src/edu/kh/control/practice/LoopPractice.java
+++ src/edu/kh/control/practice/LoopPractice.java
@@ -13,8 +13,134 @@ public class LoopPractice {
 		System.out.print("1이상의 숫자를 입력하세요 : ");
 		int input = sc.nextInt();
 		
-		for()
+		for(int i = 1; i <= input; i++) {
+			System.out.print(i + "\s");
+			
+			if(i < 0) {
+				System.out.println("1 이상의 숫자를 입력해주세요");
+			}
+		}	
+		
+		
+	}
+	public void pr2() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int input = sc.nextInt();
+		
+		for(int i = input; i > 0; i--) {
+			
+			if(input >= 1) {
+				System.out.print(i + "\s");
+				
+				
+			} else {
+				System.out.println("1이상의 숫자를 입력");
+			
+			}
+	
+		}
+	}	
+	
+	public void pr3() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int input = sc.nextInt();
+		
+		int sum = 0;
+		
+		for(int i = 0; i <= input; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+
+	public void pr4() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("첫번째 숫자 : ");
+		int input = sc.nextInt();
+		
+		System.out.print("두번쨰 숫자 : ");
+		int input2 = sc.nextInt();
+		
+		for(int i = input2; i < input; i++ ) {
+			if(i > 0) {
+				System.out.print(i);
+			} else { 
+				System.out.print("1이상의 숫자를 입력해주세요");
+			}
+				
+			
+			
+		}
+	}
+	
+	
+	public void pr5() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 : ");
+		int input = sc.nextInt();
+		 
+		System.out.print("===== "  + input + "단 =======");
+		 for(int i2 = 1; i2 <= 9; i2++ ) {
+				int i = input;
+				
+				System.out.println();
+				System.out.printf("%2d X %2d = %2d", i ,i2 , i * i2);
+				System.out.println();
+			}
+
+		
+	}
+	public void pr6 () {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 : ");
+		int input = sc.nextInt();
+		
+		for(int i = input; i <= 9; i++) {
+			for(int i2 = 1; i2 <= 9; i2++) {
+				
+				if(i > 1) {
+					System.out.printf("%2d X %2d = %2d", i ,i2 , i * i2);
+					System.out.println();
+					
+				} else if(i < 10 ) {
+					
+				
+				System.out.printf("%2d X %2d = %2d", i ,i2 , i * i2);
+				System.out.println();
+					}else {
+						System.out.println("맞는 숫자를입력");
+					}
+				
+			}
+			
+			
+			}
+		
+		
+	}
+	
+	public void pr7() {
+		
+		
+		//감도 안옵니다 선생님
 		
 	}
 
 }
+
+
+
+
--- src/edu/kh/control/practice/Run2.java
+++ src/edu/kh/control/practice/Run2.java
@@ -8,7 +8,7 @@ public class Run2 {
 		LoopPractice lp = new LoopPractice();
 		
 		
-		lp.pr1();
+		lp.pr6();
 		
 				
 
