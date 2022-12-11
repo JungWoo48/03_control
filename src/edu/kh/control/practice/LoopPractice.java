f0c8b70bea0be8abe77a5f182c8caebd1cd92096
DESKTOP-HE6SEOU\arm <suj000777@gmail.com>
2022-12-11 19:51:15 +0900

    comiit 4

---
+++ src/edu/kh/control/practice/LoopPractice.java
@@ -0,0 +1,20 @@
+package edu.kh.control.practice;
+
+import java.util.Scanner;
+
+public class LoopPractice {
+	
+	
+	
+	public void pr1() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int input = sc.nextInt();
+		
+		for()
+		
+	}
+
+}
